183c60a5586fb88a710b2d9823b28e51c922ee5b
dan <dancr.wley@gmail.com>
2023-05-25 19:39:52 -0400

    m

--- lib/teacher.py
+++ lib/teacher.py
@@ -6,5 +6,13 @@ import random
 
 class Teacher(User):
 
-    def teach(self):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ['mirage easy how to a split', 'pipebomb tutorial', 'scam vending machine', 'art of the respect', 'ap california', 'blood sacrifice']
+
+    def log_in(self):
         pass
+
+    def teach(self):
+        one_piece_of_esoteric_knowledge_please = random.choice(self.knowledge)
+        return one_piece_of_esoteric_knowledge_please
